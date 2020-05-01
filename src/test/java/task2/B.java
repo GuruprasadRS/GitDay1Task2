16251c350b9a87bf08b904f963c80355abe4e3f8
GuruprasadRS <guruprasad.off.use@gmail.com>
2020-05-01 13:48:49 +0530

    2nd Project

---
+++ src/test/java/task2/B.java
@@ -0,0 +1,14 @@
+package task2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class B {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Guru\\Maven-Workspace\\GitDay1Task1\\driver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://twitter.com/login");
+		driver.manage().window().maximize();
+		driver.quit();
+	}
+}
